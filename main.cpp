1478a16ba0eb56301b69b81406f33e2402d625a9
m B <downrightmike@gmail.com>
2022-11-19 02:59:59 +0000

    2d Magic square

--- main.cpp
+++ main.cpp
@@ -8,30 +8,46 @@ fill the array with the numbers a function to determine if the array is a magic
 square. Run these functions for some large number of times, say 1,000, 10,000,
 or 1,000,000, and see the number of times the array is a magic square.
 */
-// Two-dimensional arrays as parameters to functions.
-#include <iomanip>               //Line 2
-#include <iostream>              //Line 1
-using namespace std;             // Line 3
-const int NUMBER_OF_ROWS = 6;    // Line 4
-const int NUMBER_OF_COLUMNS = 5; // Line 5
+
+#include <iomanip>             
+#include <iostream>              
+using namespace std;           
+const int NUMBER_OF_ROWS = 6;    
+const int NUMBER_OF_COLUMNS = 5; 
 void printMatrix(int matrix[][NUMBER_OF_COLUMNS],
-                 int NUMBER_OF_ROWS); // Line 6
+                 int NUMBER_OF_ROWS); 
 void sumRows(int matrix[][NUMBER_OF_COLUMNS],
-             int NUMBER_OF_ROWS); // Line 7
+             int NUMBER_OF_ROWS);
+int sumRowsCheck(int matrix[][NUMBER_OF_COLUMNS],
+             int NUMBER_OF_ROWS); 
 void largestInRows(int matrix[][NUMBER_OF_COLUMNS],
-                   int NUMBER_OF_ROWS); // Line 8
-int main()                              // Line 9
-{                                       // Line 10
-  int board[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
-      {17, 8, 24, 10, 28}, {9, 20, 16, 55, 90}, {25, 45, 35, 8, 78},
-      {5, 0, 96, 45, 38},  {76, 30, 8, 14, 28}, {9, 60, 55, 62, 10}}; // Line 11
-  printMatrix(board, NUMBER_OF_ROWS);                                 // Line 12
-  cout << endl;                                                       // Line 13
-  sumRows(board, NUMBER_OF_ROWS);                                     // Line 14
-  cout << endl;                                                       // Line 15
-  largestInRows(board, NUMBER_OF_ROWS);                               // Line 16
-  return 0;                                                           // Line 17
-} // Line 18
+                   int NUMBER_OF_ROWS);
+
+int main()                              
+{                                       
+  srand(time(0));
+  for(int i = 0; i < 10000; i++){
+  int board[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
+      for(int r = 0; r < NUMBER_OF_ROWS; r++){
+        for(int c = 0; c < NUMBER_OF_COLUMNS; c++){
+          board[r][c] = rand() % 10;
+        }
+      }
+  //need to check if rows ==
+    int check = sumRowsCheck(board, NUMBER_OF_ROWS);
+   if( check == 0){
+  printMatrix(board, NUMBER_OF_ROWS);                                 // print the board
+  cout << endl;                                                       // 
+  sumRows(board, NUMBER_OF_ROWS);                                     // Sum of rows
+  sumRowsCheck(board, NUMBER_OF_ROWS);                                // Sum verify
+  cout << endl;                                                       // 
+  largestInRows(board, NUMBER_OF_ROWS);                               // Largest int found
+    } 
+   // else{ cout << "No Magic here." << endl;}
+    }// end of i loop
+  
+  return 0;                                                           //
+} // 
 // Place the definitions of the functions printMatrix,
 // sumRows, and largestInRows as described previously here.
 void largestInRows(int matrix[][NUMBER_OF_COLUMNS], int noOfRows) {
@@ -56,6 +72,33 @@ void sumRows(int matrix[][NUMBER_OF_COLUMNS], int noOfRows) {
     for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
       sum = sum + matrix[row][col];
     cout << "Sum of row " << (row + 1) << " = " << sum << endl;
+  }
+}
+int sumRowsCheck(int matrix[][NUMBER_OF_COLUMNS], int noOfRows) {
+  int sum;
+  int sling[noOfRows];
+  // Sum of each individual row
+  for (int row = 0; row < noOfRows; row++) {
+    sum = 0;
+    for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
+      sum = sum + matrix[row][col];
+      sling[row] = sum;
+  }
+  /*for(int num = 0; num < 6; num++){
+  cout << sling[num] << " ";
+    }  cout << endl;*/
+  if(sling[0] != sling[1]){
+    return 1;
+  }else if(sling[0] != sling[2]){
+    return 1;
+  }else if(sling[0] != sling[3]){
+    return 1;
+  }else if(sling[0] != sling[4]){
+    return 1;
+  }else if(sling[0] != sling[5]){
+    return 1;
+  }else{
+    return 0;
   }
 }
 void printMatrix(int matrix[][NUMBER_OF_COLUMNS], int noOfRows) {

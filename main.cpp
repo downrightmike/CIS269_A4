337a408d8ed898b6e84d0dedf74c10e0b0daa20c
m B <downrightmike@gmail.com>
2022-11-27 23:14:45 +0000

    Finalized

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-/*Write a program that uses a 3 3 3 array and randomly place each
-integer from 1 to 9 into the nine squares.
-The program calculates the magic number by adding all the numbers in the array
-and then dividing the sum by 3. The 3 3 3 array is a magic square if the sum of
-each row, each column, and each diagonal is equal to the magic number. Your
-program must contain at least the following functions: a function to randomly
-fill the array with the numbers a function to determine if the array is a magic
-square. Run these functions for some large number of times, say 1,000, 10,000,
-or 1,000,000, and see the number of times the array is a magic square.
+/*Magic square program that creates squares with random numbers and then sees if they are magic.
 */
 
 #include <iomanip>             
